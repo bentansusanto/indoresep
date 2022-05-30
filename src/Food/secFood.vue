48eab73ba143ba91b3dd170f13a3eed83c97b236
bentansusanto <tansusanto194@gmail.com>
2022-05-31 00:15:43 +0700

    update food page

---
+++ src/Food/secFood.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="input-group rounded w-25 mx-auto my-5">
+        <input type="search" class="form-control rounded" placeholder="Search"  />
+        <span class="input-group-text border-0" id="search-addon">
+            <img src="https://img.icons8.com/ios-filled/25/090909/search--v1.png"/>
+        </span>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
--- src/views/foodView.vue
+++ src/views/foodView.vue
@@ -1,3 +1,43 @@
 <template>
-    <p></p>
+    <div class="foods">
+        <div class="container">
+            <h1 class="text-center">Kumpulan Makanan Khas<br>Batam dan Resepnya</h1>
+        </div>
+    </div>
+    <secFood/>
 </template>
+<script>
+import secFood from '../Food/secFood.vue'
+export default {
+    components: {
+        secFood
+    },
+}
+</script>
+<style scoped>
+    .foods{
+        background: url('../assets/img-food/bg-food.jpg');
+        width: 85%;
+        height:80vh;
+        background-size: cover;
+        background-position: center;
+        margin: 0 auto;
+        padding: 300px 0;
+        color: #fff;
+    }
+    .foods h1{
+        font-weight: 600;
+    }
+    /* Large Desktop */
+    @media only screen and (min-width: 1025px){
+        
+    }
+    /* Tablet and Desktop */
+    @media only screen and (max-width: 104px){
+
+    }
+    /* Handphone */
+    @media only screen and (min-width: 767px){
+
+    }
+</style>
